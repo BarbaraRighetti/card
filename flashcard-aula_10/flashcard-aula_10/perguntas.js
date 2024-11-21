b007ac6e8517ee304c7f8787d063586d68844f01
BarbaraRighetti <righettib8@gmail.com>
2024-11-21 09:03:40 -0300

    Add files via upload

---
+++ flashcard-aula_10/flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'curiosidade',
+    'primeira pessoa a descobrir o espaço',
+    'Yuri Gagarin'
+)
+
+criaCartao(
+    'geografia curiosa',
+    'a montanha mais alta o mundo é',
+    'monte everest'
+)
+
+criaCartao(
+    'fisica/quimica',
+    'qual a temperatura que a água ferve?',
+    '100°C'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz eu te amo em Inglês?',
+    'eu te amo em inglês se fala I love you (ai love iu)'
+)
